18be1d38305cf8a954b5714198e7bcda8873cc73
se-yong <parksae0428@gmail.com>
2022-08-11 13:34:28 +0900

    custom pipe를 이용한 유효성 체크 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,19 +1,21 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto'
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) { }
 
-    @Get()
+    @Get('/')
     getAllBoard(): Board[] {
         return this.boardsService.getAllBoards();
     }
 
 
     @Post()
+    @UsePipes(ValidationPipe)
     createBoard(
         @Body() createBoardDto: CreateBoardDto
     ): Board {
@@ -33,7 +35,7 @@ export class BoardsController {
     @Patch('/:id/status')
     updateBoardStatus(
         @Param('id') id: string,
-        @Body('status') status: BoardStatus,
+        @Body('status', BoardStatusValidationPipe) status: BoardStatus,
     ) {
         return this.boardsService.updateBoardStatus(id, status)
     }
