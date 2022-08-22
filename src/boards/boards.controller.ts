35e90a944251242e547bb7d9b6982aaa4b64c4ac
se-yong <parksae0428@gmail.com>
2022-08-22 16:08:36 +0900

    데이터베이스를 이용한 CRUD 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,40 +1,45 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto'
-import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) { }
 
-    @Get('/')
-    getAllBoard(): Board[] {
+    // @Get('/')
+    // getAllBoard(): Board[] {
+    //     return this.boardsService.getAllBoards();
+    // }
+
+
+    @Get()
+    getAllBoards(): Promise<Board[]> {
         return this.boardsService.getAllBoards();
     }
 
-
     @Post()
     @UsePipes(ValidationPipe)
-    createBoard(
-        @Body() createBoardDto: CreateBoardDto
-    ): Board {
+    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
         return this.boardsService.createBoard(createBoardDto);
     }
 
     @Get('/:id')
-    getBoardById(@Param('id') id: string): Board {
-        return this.boardsService.getBoardById(id)
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardsService.getBoardById(id);
     }
 
     @Delete('/:id')
-    deleteBoard(@Param('id') id: string): void {
-        this.boardsService.deleteBoard(id);
+    deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
+        return this.boardsService.deleteBoard(id);
     }
 
     @Patch('/:id/status')
     updateBoardStatus(
-        @Param('id') id: string,
+        @Param('id', ParseIntPipe) id: number,
         @Body('status', BoardStatusValidationPipe) status: BoardStatus,
     ) {
         return this.boardsService.updateBoardStatus(id, status)
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -7,7 +7,7 @@ export const typemORMConfig: TypeOrmModuleOptions = {
     username: 'postgres',
     password: 'postgres',
     database: 'board-app',
-    entities: [__dirname + '/../**/*.entity/{js,ts}'],
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
     //synchronize: true값을 주면 애플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이, 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해줍니다.
 }
