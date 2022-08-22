c5aa3fbdf0be4dd1a6ebf7b6fd5757ef27f3984d
se-yong <parksae0428@gmail.com>
2022-08-22 18:43:33 +0900

    유저이름에 유니크한 값 추가

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -7,8 +7,8 @@ export class AuthCredentialsDto {
     username: string;
 
     @IsString()
-    @MaxLength(4)
-    @MinLength(20)
+    @MinLength(4)
+    @MaxLength(20)
     // 영어랑 숫자조합으로 password 유효성체크
     @Matches(/^[a-zA-Z0-9]*$/, {
         message: "invalid  password: password only accepts english and number",
