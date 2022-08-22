ad3bb0bc61219a6bce8d9d700d0cfa7112729420
se-yong <parksae0428@gmail.com>
2022-08-22 18:10:38 +0900

    유저 데이터 유효성 체크

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MaxLength(4)
+    @MinLength(20)
+    // 영어랑 숫자조합으로 password 유효성체크
+    @Matches(/^[a-zA-Z0-9]*$/, {
+        message: "invalid  password: password only accepts english and number",
+    })
     password: string;
 }
