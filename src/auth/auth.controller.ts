f798ea89915c66812d5268b1e43e3ffb7a849fe3
parkseyong <parksae0428@gmail.com>
2022-10-06 16:50:46 +0900

    [ADD]: JWT를 이용해서 토큰 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
     }
 
     @Post('/signin')
-    signIn(@Body(ValidationPipe) authcredentialsDto: AuthCredentialsDto) {
+    signIn(@Body(ValidationPipe) authcredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         return this.authService.signIn(authcredentialsDto);
     }
 }
